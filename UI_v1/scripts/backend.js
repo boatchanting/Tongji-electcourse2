82c6625cf8dc3657b365f1aeddb77119079cd0da
boatchanting <162798997+boatchanting@users.noreply.github.com>
2025-05-26 12:24:42 +0800

    弹框链接化

--- UI_v1/scripts/backend.js
+++ UI_v1/scripts/backend.js
@@ -3,12 +3,10 @@
 // ----------------------------------
 let currentLogTask = null, logTimer = null;
 
-// 显示错误 Modal
 function showErrorModal(msg) {
-  document.getElementById('modal-error-body').textContent = msg;
+  document.getElementById('modal-error-body').innerHTML = msg;
   document.getElementById('modal-error').classList.add('active');
 }
-// 隐藏错误 Modal
 function hideErrorModal() {
   document.getElementById('modal-error').classList.remove('active');
 }
